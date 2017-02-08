d2e29b72a23637af067c51db486b68c4c7f2efd2
4010c9 <4010c9@anon.ece.uprm.edu>
2017-02-08 12:37:05 -0400

    The program has a different name, different resolution and it runs centralized on the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Lexdyel Mendez");
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int width = (int) dim.getWidth() /2 ;
+		int height =(int)  dim.getHeight() /2;
+		
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setSize(200, 200);
+		myFrame.setLocation(width - myFrame.getWidth()/2, height - myFrame.getHeight()/2);
 		myFrame.setVisible(true);
 	}
 }
